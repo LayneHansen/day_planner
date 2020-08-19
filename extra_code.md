9334c326fea03e7a7ad35b6f072f7870deb15989
LayneHansen <layne.d.hansen@gmail.com>
2020-08-18 22:34:29 -0600

    created html and css files, linked js files and moment

---
+++ extra_code.md
@@ -0,0 +1,18 @@
+<div class="col-md-1" id="left-col">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">8:00</li>
+                <li class="list-group-item">9:00</li>
+                <li class="list-group-item">10:00</li>
+                <li class="list-group-item">11:00</li>
+                <li class="list-group-item">12:00</li>
+                <li class="list-group-item">1:00</li>
+                <li class="list-group-item">2:00</li>
+                <li class="list-group-item">3:00</li>
+                <li class="list-group-item">4:00</li>
+                <li class="list-group-item">5:00</li>
+                <li class="list-group-item"></li>
+              </ul>
+
+        </div>
+
+        
---
+++ script.js
@@ -0,0 +1,42 @@
+// var date = new Date();
+// document.querySelector("#time-display").innerHTML = date.toDateString();
+
+// TIME DISPLAY
+
+var currentDay = moment();
+var displayDay = document.getElementById("time-display");
+displayDay.innerHTML = currentDay.format('LLLL');
+
+
+
+// SAVE BUTTONS
+
+// var button8 = document.querySelector("#btn8");
+// var button9 = document.querySelector("#btn9");
+// var button10 = document.querySelector("#btn10");
+// var button11 = document.querySelector("#btn11");
+// var button12 = document.querySelector("#btn12");
+// var button1 = document.querySelector("#btn1");
+// var button2 = document.querySelector("#btn2");
+// var button3 = document.querySelector("#btn3");
+// var button4 = document.querySelector("#btn4");
+// var button5 = document.querySelector("#btn5");
+
+
+// BUTTON EVENTS
+
+// button8.addEventListener('click' function ());
+// button9.addEventListener('click' function ());
+// button10.addEventListener('click' function ());
+// button11.addEventListener('click' function ());
+// button12.addEventListener('click' function ());
+// button1.addEventListener('click' function ());
+// button2.addEventListener('click' function ());
+// button3.addEventListener('click' function ());
+// button4.addEventListener('click' function ());
+// button5.addEventListener('click' function ());
+
+// var date = new Date();
+// document.getElementById("time-display").innerText = date;
+
+
