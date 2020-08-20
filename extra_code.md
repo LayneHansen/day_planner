d4edf5e586557ad915ee0680641f861b47fc86c0
LayneHansen <layne.d.hansen@gmail.com>
2020-08-19 21:14:32 -0600

    function for save button to save text in field

--- extra_code.md
+++ extra_code.md
@@ -1,4 +1,4 @@
-<div class="col-md-1" id="left-col">
+<!-- <div class="col-md-1" id="left-col">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">8:00</li>
                 <li class="list-group-item">9:00</li>
@@ -13,6 +13,25 @@
                 <li class="list-group-item"></li>
               </ul>
 
-        </div>
+        </div> -->
 
-        
+
+<!-- var userEvent = {
+
+    field8Input: field8.value.trim(),
+    field9Input: field9.value.trim(),
+    field10Input: field10.value.trim(),
+    field11Input: field11.value.trim(),
+    field12Input: field12.value.trim(),
+    field1Input: field1.value.trim(),
+    field2Input: field2.value.trim(),
+    field3Input: field3.value.trim(),
+    field4Input: field4.value.trim(),
+    field5Input: field5.value.trim(),
+}      -->
+
+<!-- // var date = new Date();
+// document.querySelector("#time-display").innerHTML = date.toDateString();
+
+// var date = new Date();
+// document.getElementById("time-display").innerText = date; -->
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// var date = new Date();
-// document.querySelector("#time-display").innerHTML = date.toDateString();
 
 // TIME DISPLAY
 
@@ -7,11 +5,23 @@ var currentDay = moment();
 var displayDay = document.getElementById("time-display");
 displayDay.innerHTML = currentDay.format('LLLL');
 
+var field8Event;
 
 
-// SAVE BUTTONS
 
-// var button8 = document.querySelector("#btn8");
+var field8 = document.querySelector("#text-area-1");
+// var field9 = document.querySelector("#text-area-2");
+// var field10 = document.querySelector("#text-area-3");
+// var field11 = document.querySelector("#text-area-4");
+// var field12 = document.querySelector("#text-area-5");
+// var field1 = document.querySelector("#text-area-6");
+// var field2 = document.querySelector("#text-area-7");
+// var field3 = document.querySelector("#text-area-8");
+// var field4 = document.querySelector("#text-area-9");
+// var field5 = document.querySelector("#text-area-10");
+
+
+var button8 = document.querySelector("#btn8");
 // var button9 = document.querySelector("#btn9");
 // var button10 = document.querySelector("#btn10");
 // var button11 = document.querySelector("#btn11");
@@ -22,10 +32,39 @@ displayDay.innerHTML = currentDay.format('LLLL');
 // var button4 = document.querySelector("#btn4");
 // var button5 = document.querySelector("#btn5");
 
+// function fieldInput(type, message) {
+//     field8.innerText = message;
+//     localStorage.setItem("Event", type);
+    
+// }
+
 
 // BUTTON EVENTS
 
-// button8.addEventListener('click' function ());
+button8.addEventListener('click', function () {
+    // console.log(button8);
+    
+    field8Event = field8.value;
+    // console.log(field8Event);
+    localStorage.setItem("Event", field8Event);
+    
+});
+
+field8.innerHTML = localStorage.getItem("Event");
+
+
+// $("btn8").on("click", function() {
+//     field8Event = field8.val;
+//     localStorage.setItem("Event", field8Event);
+
+// })
+
+// field8.innerHTML = localStorage.getItem("Event");
+
+
+// fieldInput();
+
+
 // button9.addEventListener('click' function ());
 // button10.addEventListener('click' function ());
 // button11.addEventListener('click' function ());
@@ -36,7 +75,6 @@ displayDay.innerHTML = currentDay.format('LLLL');
 // button4.addEventListener('click' function ());
 // button5.addEventListener('click' function ());
 
-// var date = new Date();
-// document.getElementById("time-display").innerText = date;
+
 
 
